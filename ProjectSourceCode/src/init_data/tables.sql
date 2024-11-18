68dddb8a6a7f487652eb85c6236322612299446d
Ayush Khadka <ayushkhadka514@gmail.com>
2024-11-17 17:28:39 -0700

    able to regsiter

--- ProjectSourceCode/src/init_data/tables.sql
+++ ProjectSourceCode/src/init_data/tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "user" (
+CREATE TABLE "users" (
   user_id SERIAL PRIMARY KEY /* the primary key for each entry */,
   username VARCHAR(100) NOT NULL,
   fullname VARCHAR(100) NOT NULL,
