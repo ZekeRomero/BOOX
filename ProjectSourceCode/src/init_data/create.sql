ffb62be40d321cabb9139832942c8f7970eea12f
Charlie Hewitt <charles.hewitt@colorado.edu>
2024-11-18 22:45:25 -0700

    Updating databases for reviews and comments

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -23,3 +23,21 @@ CREATE TABLE user_collections (
     book_isbn VARCHAR(13) NOT NULL
 )
 
+CREATE TABLE reviews (
+  review_id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL,
+  id INT NOT NULL,
+  rating INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (user_id), /* ensure referential integrity */ 
+  FOREIGN KEY (id) REFERENCES isbns (id)
+);
+
+CREATE TABLE comments (
+    comment_id SERIAL PRIMARY KEY,
+    review_id INT NOT NULL,  -- Foreign key referencing reviews
+    user_id INT NOT NULL,    -- Foreign key referencing users
+    comment TEXT NOT NULL,   -- The comment text
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  -- Timestamp for when the comment was created
+    FOREIGN KEY (review_id) REFERENCES reviews (review_id) ON DELETE CASCADE,  -- On review deletion, delete the comments
+    FOREIGN KEY (user_id) REFERENCES user (user_id)  -- Reference to the user who made the comment
+);
